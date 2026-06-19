9720a95b443a3877a81d4a5688741bc049d95084
pabloveramsanjose <pabloveram.sanjosemlg@fundacionloyola.net>
2026-06-19 09:42:33 +0200

    Update Vehiculo.java

--- ExamenRA2y3ENDE/src/enunciado/Vehiculo.java
+++ ExamenRA2y3ENDE/src/enunciado/Vehiculo.java
@@ -1,3 +1,8 @@
+/**
+ * @author: Pablo Vera Martinez
+ * @date: 19/06/2026
+ * @version: 1.0.0
+ */
 package enunciado;
 
 public abstract class Vehiculo {
@@ -7,8 +12,15 @@ public abstract class Vehiculo {
     protected double precioBase;
     protected boolean electrico;
     protected float tasaEmisiones;
-
+    /**
+     * 
+     * @param modelo: Modelo del vehiculo
+     * @param precioBase: Precio base del vehiculo
+     * @param electrico: Indica si el electrico o no
+     * @param tasaEmisiones: Tasa de emisiones del vehiculo
+     */
     // Constructor completo
+    
     public Vehiculo(String modelo, double precioBase, Boolean electrico, float tasaEmisiones) {
         if (precioBase < PRECIO_BASE_MINIMO) {
             throw new IllegalArgumentException("El precio base no puede ser inferior al mínimo permitido");
@@ -18,6 +30,11 @@ public abstract class Vehiculo {
         this.electrico = electrico;
         this.tasaEmisiones = tasaEmisiones;
     }
+    
+    /**
+     * Constructor  creado para la clase de Vehiculos con excepciones
+     * @param modelo
+     */
 	
     // Constructor simplificado
     public Vehiculo(String modelo) {
@@ -26,6 +43,11 @@ public abstract class Vehiculo {
         this.electrico = false;
         this.tasaEmisiones = 0;
     }
+    
+    /**
+     * Constructor creado para la clase Vehiculo sin excepcion por lo que esta simplificada
+     * @return
+     */
 	
     // Getters
     public String getModelo() {
@@ -61,11 +83,32 @@ public abstract class Vehiculo {
         this.tasaEmisiones = tasaEmisiones;
     }
 	
+    /**
+     * 
+     * @param fianza: Precio que debe de dar el comprador si le pasa algo al vehiculo
+     * @param numDias: Indica el numero de dias
+     * Este metodo lo que hace es calcular el precio bruto sumando el precio base con la fianza entre el numero de dias
+     * Si la fianza es negativa y los dias son mayores son mayores que 12 o menor que 1, lanzara una excepcion
+     * @return
+     */
     public double calcularPrecioBruto(double fianza, int numDias) {
+    	
     	double precioBruto = precioBase+fianza/numDias;
     	
+    	if(fianza < 0) {
+    		throw new IllegalArgumentException("La fianza no puede ser negativa");
+    	}
+    	
+    	if(numDias < 1 && numDias >12 ) {
+    		throw new IllegalArgumentException("El numero de dias no puede ser menor que 1 ni mayor que 12");
+
+    	}
     	return precioBruto;
     }
     
+    /**
+     * Metodo abstracto llamado calcularPrecioNeto
+     * @return
+     */
     public abstract double calcularPrecioNeto();
 }
