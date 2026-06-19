a0072203bf09c94a641a23473f3952859a8b4b35
pabloveramsanjose <pabloveram.sanjosemlg@fundacionloyola.net>
2026-06-19 09:48:54 +0200

    Update Moto.java

--- ExamenRA2y3ENDE/src/enunciado/Moto.java
+++ ExamenRA2y3ENDE/src/enunciado/Moto.java
@@ -1,11 +1,33 @@
+/**
+ * @author: Pablo Vera
+ * @date: 19/06/2026
+ * @version:1.0.0
+ * Clase Moto creada a partir  de la clase Coche
+ */
 package enunciado;
+
 
 public class Moto extends Coche{
 
+	/**
+	 * 
+	 * @param modelo: Modelo de la moto
+	 * @param precioBase: Precio base de la moto
+	 * @param electrico: Si es electrico o no
+	 * @param tasaEmisiones: tasa de emisiones de la moto
+	 * @param fianza: fianza de la moto
+	 * @param numDias: Numero de dias que tiene la moto
+	 */
 	public Moto(String modelo, double precioBase, Boolean electrico, float tasaEmisiones, double fianza, int numDias) {
 		super(modelo, precioBase, electrico, tasaEmisiones, fianza, numDias);
 	}
+	/**
+	 * Constructor creado para la clase moto
+	 */
 	
+	/**
+	 * Metodo creado para calcular el precio neto de la moto, si no es electrico calcula normal el precio neto, si es electrico se calculara igual pero sumandole ele 7%
+	 */
 	@Override
 	public double calcularPrecioNeto() {
 
@@ -18,7 +40,13 @@ public class Moto extends Coche{
 	}
 
 	
-
+/**
+ * 
+ * @param extraKilometraje: variable creada para sumar valor a la moto
+ * metodo creado para calcular el precio bruto sumando el precio base + el valor del kilometraje
+ * si es menor que 0 el valor, lanzara una excepcion
+ * @return
+ */
 	public double calcularPrecioBruto(double extraKilometraje) {
 		
 		double precioBruto = precioBase + extraKilometraje;
@@ -31,9 +59,21 @@ public class Moto extends Coche{
 		return precioBruto;
 	}
 
+
+	/**
+	 * Metodo creado para calcular el precio neto de la moto, si no es electrico calcula normal el precio neto, si es electrico se calculara igual pero sumandole ele 7%
+	 */
 	public double calcularPrecioNeto(double extraKilometraje) {
 
 		if(electrico == false) {
+		return calcularPrecioBruto(extraKilometraje) - calcularPrecioBruto(extraKilometraje);
+		
+		}else{
+			return calcularPrecioBruto(extraKilometraje) - calcularPrecioBruto(extraKilometraje)*0.07;
+		}
+	}
+
+}
 		return calcularPrecioBruto(extraKilometraje) - calcularPrecioBruto(extraKilometraje);
 		
 		}else{
